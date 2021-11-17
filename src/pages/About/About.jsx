67401d5f409c3d061aa17b7586b213a22ae5668e
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-11-17 14:41:15 +0200

    about page with photo

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,13 +1,48 @@
 import React from "react";
-import { H3 } from "./About.ui";
+import "./About.css";
 
 function About() {
-    return (
-        <>
-        <H3>About</H3>
-        <H3>Name</H3>
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <div class="profile">
+          <div class="profile-image">
+            <img
+              src="https://images.unsplash.com/photo-1513721032312-6a18a42c8763?w=152&h=152&fit=crop&crop=faces"
+              alt=""
+            />
+          </div>
+
+          <div class="profile-user-settings">
+            <h1 class="profile-user-name">user_nickname_</h1>
+
+            <button class="btn profile-edit-btn">Edit Profile</button>
+
+            <button
+              class="btn profile-settings-btn"
+              aria-label="profile settings"
+            >
+              <i class="fas fa-cog" aria-hidden="true"></i>
+            </button>
+          </div>
+
+          <div class="profile-stats">
+            <ul>
+              <li>
+                <span class="profile-stat-count">164</span> posts
+              </li>
+              <li>
+                <span class="profile-stat-count">188</span> followers
+              </li>
+              <li>
+                <span class="profile-stat-count">206</span> following
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
--- src/pages/About/About.ui.js
+++ src/pages/About/About.ui.js
@@ -4,16 +4,3 @@ export const HeaderRoot = styled.div`
     padding: 1rem;
     background-color: bisque;
 `;
-
-// export const Li = styled.li`
-//     display: inline-block;
-//     padding: 1em;
-//     color: brown;
-//     text-transform: uppercase;
-//     font: bold 16px/2 cursive;
-// ` ;
-
-export const H3 = styled.h3 `
-    padding: 1rem;
-    text-align: center
-`;
