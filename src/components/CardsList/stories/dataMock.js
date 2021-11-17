5c8f710f9ccb1710c16ca9001836f418271795dc
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-11-17 13:58:38 +0200

    front page

--- src/components/CardsList/stories/dataMock.js
+++ src/components/CardsList/stories/dataMock.js
@@ -5,7 +5,7 @@ export const defaultData = [
         avatar: `https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80`,
         title: 'Nick Name',
         likes: '123',
-        postText: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis placeat, molestiae impedit aut provident eum quo natus molestias?',
+        postText: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis plLorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis placeat, molestiae impedit aut provident eum quo natus molestias?',
         image: `https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80`
     },
     {
@@ -19,25 +19,5 @@ export const defaultData = [
         likes: '23',
         postText: 'Hello it is post Text',
         image: `https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80`
-    },
-    // {
-    //     nick: 'Some card 3',
-    //     price: '1235UAH',
-    //     image: verticalImage
-    // },
-    // {
-    //     nick: 'Some card 4',
-    //     price: '23UAH',
-    //     image: wideImage
-    // },
-    // {
-    //     nick: 'Some card 5',
-    //     price: '18UAH',
-    //     image: verticalImage
-    // },
-    // {
-    //     nick: 'Some card 9',
-    //     price: '123UAH',
-    //     image: wideImage
-    // },
+    }
 ]
